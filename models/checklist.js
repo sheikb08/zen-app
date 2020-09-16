cf91ae810c1d52cbcdab6156d1a31bf802a3c33d
Max Nicolai <maxvnicolai@gmail.com>
2020-09-16 14:57:12 -0700

    built out first take on checklist model

---
+++ models/checklist.js
@@ -0,0 +1,24 @@
+module.exports = function ( sequelize, DataTypes){
+  const Checklist = sequelize.define('Checklist', {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0
+    },
+    hidden: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0
+    },
+    user_id: {
+      type: DataTypes.INTEGER;
+      references: {
+        model: User,
+        key: 'id'
+      }
+    }
+  });
+};
+  
