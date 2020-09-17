738974c3b4b091647b8df070e556fde5efbf6e67
Max Nicolai <maxvnicolai@gmail.com>
2020-09-17 15:17:00 -0700

    changed enforment of camelCase, restored user_id

--- models/checklist.js
+++ models/checklist.js
@@ -12,7 +12,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.BOOLEAN,
       defaultValue: 0
     },
-    userId: {
+    user_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "Users",
