58fee3c64a9af583a1885583164870261c2cd2bc
Max Nicolai <maxvnicolai@gmail.com>
2020-09-18 16:06:14 -0700

    added err catches, clarified comments, cleaned code.

--- routes/checklist-routes.js
+++ routes/checklist-routes.js
@@ -1,47 +1,55 @@
 const db = require("../models");
 
 module.exports = function(app) {
-  //GET ALL checklist
+  //GET ALL checklist items
   app.get("/api/list", (req, res) => {
-    console.log("test g all");
-    db.Checklist.findAll({}).then(data => {
-      res.status(200);
-      res.json(data);
-    });
+    db.Checklist.findAll({})
+      .then(data => {
+        //Currently returns list, may switch to res.render for HBS
+        res.status(200).json(data);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
   });
 
   //GET checklist item
   app.get("/api/list/:id", (req, res) => {
-    console.log("test get");
-    let id = req.params.id;
-    db.Checklist.findAll({ where: { id: req.params.id } }).then(data => {
-      res.status(200);
-      res.json(data);
-    });
+    db.Checklist.findAll({ where: { id: req.params.id } })
+      .then(data => {
+        //As above, could be res.render, or location.reload, or something else
+        res.status(200).json(data);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
   });
 
-  //POST checklist
+  //POST checklist item
   app.post("/api/list", (req, res) => {
-    console.log("check post");
     db.Checklist.create({
-      body: req.body.body,
-      //How should we pull user id for this?
+      //Assuming body.value is pulled from todo text entry field
+      body: req.body.value,
+      //How should we get user's id into body for this?
       user_id: req.body.user
-    }).then(data => {
-      //May not need to return data, could call render here
-      res.status(200);
-      res.json(data);
-    });
+    })
+      .then(data => {
+        //May not need to return data, also see above
+        res.status(200).json(data);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
   });
 
-  //UPDATE checklist
+  //UPDATE checklist item as complete
   app.put("/api/list/:id", (req, res) => {
-    console.log("check put");
     db.Checklist.update(
       {
         completed: 1,
-        //Should below be a separate call? probably-prevent accidents.
-        //Implement on page how?
+        //Should below be a separate call/route (/complete)?
+        //Might be difficult to reverse accidental 'deletion' otherwise.
+        //If so: how to implement on page (and in backend)?
         hidden: 1
       },
       {
@@ -49,10 +57,13 @@ module.exports = function(app) {
           id: id
         }
       }
-    ).then(data => {
-      //As above, res might be be better off changed post testing
-      res.status(200);
-      res.json(data);
-    });
+    )
+      .then(data => {
+        //As above, decide how to handle this code post testing/html dev
+        res.status(200).json(data);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
   });
 };
