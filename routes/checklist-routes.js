d106a2290c4ef77996267731c18f38d0c3eb75cf
Max Nicolai <maxvnicolai@gmail.com>
2020-09-17 21:25:00 -0700

    Added logic to routes, possibly complete: needs testing

--- routes/checklist-routes.js
+++ routes/checklist-routes.js
@@ -4,20 +4,55 @@ module.exports = function(app){
   //GET ALL checklist
   app.get("api/list", (req, res) => {
     console.log("test g all");
+    db.Checklist.findAll({}).then(data => {
+      res.status(200);
+      res.json(data);
+    });
   });
 
   //GET checklist item
   app.get("api/list/:id", (req, res) => {
-    console.log("test get")
+    console.log("test get");
+    let id = req.params.id;
+    db.Checklist.findAll({ where: { id: req.params.id } }).then(data => {
+      res.status(200);
+      res.json(data);
+    });
   });
 
   //POST checklist
   app.post("api/list", (req, res) => {
     console.log("check post");
+    db.Checklist.create({
+      body: req.body.body,
+      //How should we pull user id for this?
+      user_id: req.body.user
+    }).then(data => {
+      //May not need to return data, could call render here
+      res.status(200);
+      res.json(data);
+    });
   });
 
   //UPDATE checklist
   app.put("api/list/:id", (req, res) => {
     console.log("check put");
+    db.Checklist.update(
+      {
+        completed: 1,
+        //Should below be a separate call? probably-prevent accidents.
+        //Implement on page how?
+        hidden: 1
+      },
+      {
+        where: {
+          id: id
+        }
+      }
+    ).then(data => {
+      //As above, res might be be better off changed post testing
+      res.status(200);
+      res.json(data);
+    });
   });
 };
