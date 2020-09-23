9916ac6acebee6685199373b50965a8a73c901f9
Chien81 <cheyennehunt78@gmail.com>
2020-09-22 19:54:35 -0700

    fixed errors

--- public/js/index.js
+++ public/js/index.js
@@ -3,60 +3,60 @@ const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
-signupBtn.onclick = (()=>{
+signupBtn.onclick = () => {
   loginForm.style.marginLeft = "-50%";
   loginText.style.marginLeft = "-50%";
-});
-loginBtn.onclick = (()=>{
+};
+loginBtn.onclick = () => {
   loginForm.style.marginLeft = "0%";
   loginText.style.marginLeft = "0%";
-});
-signupLink.onclick = (()=>{
+};
+signupLink.onclick = () => {
   signupBtn.click();
   return false;
-});
+};
 
 $(document).ready(() => {
-    const loginForm = $("form.login");
-    const emailInput = $("input#email-input");
-    const passwordInput = $("input#password-input");
-    const signupForm = $("signup.form");
-    const firstnameInput = $("input#firstname-input");
-    const lastnameInput = $("input#lastname-input");
-    const emailsignupInput = $("input#emailsignup-input");
-    const passwordsignupInput = $("input#passwordsign-input");
+  const loginForm = $("form.login");
+  const emailInput = $("input#email-input");
+  const passwordInput = $("input#password-input");
+  const signupForm = $("signup.form");
+  const firstnameInput = $("input#firstname-input");
+  const lastnameInput = $("input#lastname-input");
+  const emailsignupInput = $("input#emailsignup-input");
+  const passwordsignupInput = $("input#passwordsign-input");
 
-    loginForm.on("submit", event => {
-        event.preventDefault();
-        const userData = {
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim()
-        };
+  loginForm.on("submit", event => {
+    event.preventDefault();
+    const userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()
+    };
 
-        if (!userData.email || !userData.password) {
-            return;
-        }
-
-        loginUser(userData.email, userData.password);
-        emailInput.val("");
-        passwordInput.val("");
-    });
-
-    function loginUser(email, password) {
-        $.post("/api/login", {
-            email: email,
-            password: password
-        })
-        .then(() => {
-            window.location.replace("/members");
-            // If there's an error, log the error
-          })
-          .catch(err => {
-            console.log(err);
-          });
+    if (!userData.email || !userData.password) {
+      return;
     }
 
-    signupForm.on("submit", event => {
+    loginUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  function loginUser(email, password) {
+    $.post("/api/login", {
+      email: email,
+      password: password
+    })
+      .then(() => {
+        window.location.replace("/members");
+        // If there's an error, log the error
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  signupForm.on("submit", event => {
     event.preventDefault();
     const userData = {
       firstname: firstnameInput.val().trim(),
@@ -65,31 +65,31 @@ $(document).ready(() => {
       passwordsignup: passwordsignupInput.val().trim()
     };
 
-    if (!userData.firstname || !userData.lastname || !userData.emailsignup || !userData.passwordsignup) {
+    if (!userData.firstname || !userData.lastname||!userData.emailsignup||!userData.passwordsignup) {
       return;
     }
-    signUpUser(userData.firstname, userData.lastname, userData.emailsignup, userData.passwordsignup);
+    signUpUser(userData.firstname,userData.lastname,userData.emailsignup,userData.passwordsignup);
     firstnameInput.val("");
     lastnameInput.val("");
     emailsignupInput.val("");
     passwordsignupInput.val("");
-  
-  function signUpUser(firstname, lastname, email, password) {
-    $.post("/api/signup", {
-      firstname: firstname, 
-      lastname: lastname,
-      email: email,
-      password: password
-    })
-      .then(() => {
-        window.location.replace("/login");
-      })
-      .catch(handleLoginErr);
-  }
 
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
-  }
-});
+    function signUpUser(firstname, lastname, email, password) {
+      $.post("/api/signup", {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password
+      })
+        .then(() => {
+          window.location.replace("/login");
+        })
+        .catch(handleLoginErr);
+    }
+
+    function handleLoginErr(err) {
+      $("#alert .msg").text(err.responseJSON);
+      $("#alert").fadeIn(500);
+    }
+  });
 });
