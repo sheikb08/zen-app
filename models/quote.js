923599aa31b5314c3280ab31c8a354239d398ddb
Max Nicolai <maxvnicolai@gmail.com>
2020-09-23 10:33:31 -0700

    fixed capitalization in datatypes

--- models/quote.js
+++ models/quote.js
@@ -1,15 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
   const Quote = sequelize.define("Checklist", {
     quote: {
-      type: DataTypes.String(255),
+      type: DataTypes.STRING(255),
       allowNull: false
     },
     quote_author: {
-      type: DataTypes.String(255),
+      type: DataTypes.STRING(255),
       allowNull: false
     },
     image_url: {
-      type: DataTypes.String(255),
+      type: DataTypes.STRING(255),
       allowNull: false
     }
   });
