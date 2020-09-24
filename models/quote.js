9708f90589a9fea12d10b761b08bc99f37b856b5
Max Nicolai <maxvnicolai@gmail.com>
2020-09-23 18:54:45 -0700

    Authors can now be null

--- models/quote.js
+++ models/quote.js
@@ -6,7 +6,6 @@ module.exports = function(sequelize, DataTypes) {
     },
     quote_author: {
       type: DataTypes.STRING(255),
-      allowNull: false
     },
     image_url: {
       type: DataTypes.STRING(255),
