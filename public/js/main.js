77c63b200b0f185ad8d478e89ff36dd57340e2d6
sheikb08 <sheikb08@aol.com>
2020-09-24 10:41:02 -0700

    creating js script

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+const { getQuote, getImage } = require("./api_js");
+const history = [];
+
+$(document).ready(() => {
+  function setStage() {
+    $(".quote").html(
+      "<style> background-image: url('" + getQuote() + "');</style>"
+    );
+    $(".backg").html(
+      "<style> background-image: url('" + getImage() + "');</style>"
+    );
+  }
+
+  function reflection(saveReflection){
+    $(".submit").on("click", () => {
+      saveReflection = $(".submit").val();
+      history.push(saveReflection);
+      setStage();
+    });
+  }
+
+  $(".dislike").on("click", () => {
+    setStage();
+    // clear input box
+    $(".submit").val("");
+    // clear any old content
+    $("#comment_text").empty();
+  });
+
+  $(".heart").on("click", () => {
+    // clear input box
+    $(".submit").val("");
+    // clear any old content
+    $("#comment_text").empty();
+    reflection();
+  });
+});
