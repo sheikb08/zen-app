6348817462e5acab298e355723aa9908557e78a1
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-25 22:13:13 -0700

    Fixed ESLint errors.

--- public/assets/js/check.js
+++ public/assets/js/check.js
@@ -1,198 +1,189 @@
-var UIController = function(){
+const UIController = (function() {
+  const DOMString = {
+    day: ".date--day",
+    month: ".date--month",
+    year: ".date--year",
+    addInput: ".add__input",
+    addBtn: ".add__circle",
+    taskListContainer: ".result__list",
+    listTask: ".list__task",
+    checkedBtn: ".list__task--check",
+    taskText: ".list__task--text",
+    editBtn: ".list__task--edit",
+    delBtn: ".list__task--del"
+  };
 
-    var DOMString = {
-        day: '.date--day',
-        month: '.date--month',
-        year: '.date--year',
-        addInput: '.add__input',
-        addBtn: '.add__circle',
-        taskListContainer: '.result__list',
-        listTask: '.list__task',
-        checkedBtn: '.list__task--check',
-        taskText: '.list__task--text',
-        editBtn: '.list__task--edit',
-        delBtn: '.list__task--del'
-    }
+  return {
+    testUI: function() {
+      console.log("Call from UI");
+    },
 
-    return{
-        testUI: function(){
-            console.log('Call from UI');
-        },
-        
-        getDOMString: function(){
-            return DOMString;
-        },
+    getDOMString: function() {
+      return DOMString;
+    },
 
-        getInput: function(){
-            return document.querySelector(DOMString.addInput).value;
-        },
+    getInput: function() {
+      return document.querySelector(DOMString.addInput).value;
+    },
 
-        clearInput: function(){
-            return document.querySelector(DOMString.addInput).value = "";
-        },
-        addTaskList: function(tasks){
+    clearInput: function() {
+      return (document.querySelector(DOMString.addInput).value = "");
+    },
+    addTaskList: function(tasks) {
+      const element = DOMString.taskListContainer;
 
-            var html, markup, element;
-        
-                element = DOMString.taskListContainer;
-                
-                html = '<li class="list__task" id="task-%id%"><button class="list__task--check" id="check"><i class="ion-ios-checkmark"></i></button><div class="list__task--text">%description%</div><button class="list__task--del" id="del"><i class="ion-android-delete"></i></button></li>';
-                
-            
-                markup = html.replace('%id%', tasks.id);
-                markup = markup.replace('%description%', tasks.description);
+      const html =
+        "<li class='list__task' id='task-%id%'><button class='list__task--check' id='check'><i class='ion-ios-checkmark'></i></button><div class='list__task--text'>%description%</div><button class='list__task--del' id='del'><i class='ion-android-delete'></i></button></li>";
 
-           
-                document.querySelector(element).insertAdjacentHTML("afterbegin", markup);    
-        },
+      markup = html.replace("%id%", tasks.id);
+      markup = markup.replace("%description%", tasks.description);
 
-        checkedTaskList: function(id){
-    
-            var taskID = "task-"+id;
+      document.querySelector(element).insertAdjacentHTML("afterbegin", markup);
+    },
 
-            var el = document.querySelectorAll(DOMString.listTask);
+    checkedTaskList: function(id) {
+      const taskID = "task-" + id;
 
-            for(var i = 0; i < el.length; i++){
+      const el = document.querySelectorAll(DOMString.listTask);
 
-                var listID = el[i].id;
+      for (i = 0; i < el.length; i++) {
+        const listID = el[i].id;
 
-                if(taskID === listID){
-                    
-                    el[i].childNodes[0].classList.toggle('list__task--checked');
-                    el[i].childNodes[1].classList.toggle('list__task--text--checked');
-                }
-            }   
-        },
-
-        deleteTaskList: function(id){
-            var el = document.getElementById("task-"+id);
-            el.parentNode.removeChild(el);  
-        },
-        
-        displayMonth: function(){
-            var now ,month ,year ,months ;
-
-            now = new Date;
-
-            months = ["January", "February", "March","April",
-                        "May","June","July","August",
-                        "September","October","November","December"];
-
-            day = now.getDate();
-            month = now.getMonth();
-            year = now.getFullYear();
-
-            document.querySelector(DOMString.day).textContent = day;
-            document.querySelector(DOMString.month).textContent = months[month];
-            document.querySelector(DOMString.year).textContent =  year;
+        if (taskID === listID) {
+          el[i].childNodes[0].classList.toggle("list__task--checked");
+          el[i].childNodes[1].classList.toggle("list__task--text--checked");
         }
+      }
+    },
+
+    deleteTaskList: function(id) {
+      const el = document.getElementById("task-" + id);
+      el.parentNode.removeChild(el);
+    },
+
+    displayMonth: function() {
+      const now = new Date();
+
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+
+      const day = now.getDate();
+      const month = now.getMonth();
+      const year = now.getFullYear();
+
+      document.querySelector(DOMString.day).textContent = day;
+      document.querySelector(DOMString.month).textContent = months[month];
+      document.querySelector(DOMString.year).textContent = year;
     }
-}();
+  };
+})();
 
-var TodolistController = function(){
-  
-    var data = [];
+const TodolistController = (function() {
+  const data = [];
 
-    var Task = function(id, description){
-        this.id = id,
-        this.description = description
-    }
-    
-    return{
-        testModel: function(){
-            console.log('Call from Model');
-        },
+  const Task = function(id, description) {
+    (this.id = id), (this.description = description);
+  };
 
-        createNewTask:function(desc){
-            var addItem, ID;
+  return {
+    testModel: function() {
+      console.log("Call from Model");
+    },
 
-            if(data.length > 0){
-                for(var i = 0; i < data.length; i++){
-                    ID = i + 1;
-                    addItem = new Task(ID, desc);
-                }
-            }else{
-                ID = 0;
-                addItem = new Task(ID, desc);
-            }
+    createNewTask: function(desc) {
+      let addItem, ID;
 
-            data.push(addItem);
-
-            return addItem;
-        },
-
-        deleteTask: function(id){
-            var ids, index;
-    
-            ids = data.map(function(current){
-                return current.id;
-            });
-
-            index = ids.indexOf(parseInt(id));
-            if(index !== -1){
-                data.splice(index, 1);
-            }
+      if (data.length > 0) {
+        for (i = 0; i < data.length; i++) {
+          ID = i + 1;
+          addItem = new Task(ID, desc);
         }
+      } else {
+        ID = 0;
+        addItem = new Task(ID, desc);
+      }
 
+      data.push(addItem);
+
+      return addItem;
+    },
+
+    deleteTask: function(id) {
+      const ids = data.map(current => {
+        return current.id;
+      });
+
+      const index = ids.indexOf(parseInt(id));
+      if (index !== -1) {
+        data.splice(index, 1);
+      }
     }
-}();
+  };
+})();
 
-var MainController = function(TodoCtrl,UICtrl){
-    
-    var setupEventListener = function(){
-        var DOM = UICtrl.getDOMString();
+const MainController = (function(TodoCtrl, UICtrl) {
+  const setupEventListener = function() {
+    const DOM = UICtrl.getDOMString();
 
-        
-        document.querySelector(DOM.addBtn).addEventListener('click', ctrlAdd);
-    
-        document.querySelector(DOM.addInput).addEventListener('keypress', function(e){
-            if(e.keyCode == 13 || e.which === 13){
-                ctrlAdd();
-            }
-        });
+    document.querySelector(DOM.addBtn).addEventListener("click", ctrlAdd);
 
-        document.querySelector(DOM.taskListContainer).addEventListener('click', ctrlEventCheck);
+    document.querySelector(DOM.addInput).addEventListener("keypress", e => {
+      if (e.keyCode === 13 || e.which === 13) {
+        ctrlAdd();
+      }
+    });
+
+    document
+      .querySelector(DOM.taskListContainer)
+      .addEventListener("click", ctrlEventCheck);
+  };
+
+  const ctrlAdd = function() {
+    const item = UICtrl.getInput();
+
+    if (item !== "" && item !== " ") {
+      const tasks = TodoCtrl.createNewTask(item);
+
+      UICtrl.addTaskList(tasks);
+      UICtrl.clearInput();
     }
+  };
 
-    var ctrlAdd = function(){
-        var item;
-        
-        item = UICtrl.getInput();
-        
-        if(item !== "" && item !== " "){
-            
-            var tasks = TodoCtrl.createNewTask(item);
-            
-            UICtrl.addTaskList(tasks);
-            UICtrl.clearInput();
-        }
+  const ctrlEventCheck = function(event) {
+    const itemID = event.target.parentNode.parentNode.id;
+    const itemClass = event.target.parentNode.id;
+
+    const IdSplit = itemID.split("-");
+
+    const ID = IdSplit[1];
+
+    if (itemClass === "check") {
+      UICtrl.checkedTaskList(ID);
+    } else if (itemClass === "del") {
+      TodoCtrl.deleteTask(ID);
+      UICtrl.deleteTaskList(ID);
     }
+  };
 
-    var ctrlEventCheck = function(event){
-        var itemID, itemClass, IdSplit, ID;
-
-        itemID = event.target.parentNode.parentNode.id;
-        itemClass = event.target.parentNode.id;
-
-        IdSplit = itemID.split("-");
-
-        ID = IdSplit[1];
-        
-        if(itemClass === 'check'){
-            UICtrl.checkedTaskList(ID);
-        }else if(itemClass === 'del'){
-            TodoCtrl.deleteTask(ID);
-            UICtrl.deleteTaskList(ID);
-        };
+  return {
+    init: function() {
+      console.log("script.js : connecting..");
+      setupEventListener();
+      UICtrl.displayMonth();
     }
-    
-    return{
-        init: function(){
-            console.log('script.js : connecting..');
-            setupEventListener();
-            UICtrl.displayMonth();
-        }
-    }
-}(TodolistController,UIController);
+  };
+})(TodolistController, UIController);
 
 MainController.init();
